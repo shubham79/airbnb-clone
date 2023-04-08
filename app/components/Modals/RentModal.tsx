1c1d76f31142ffe07aa944c3c6ed1abcfe67f4ba
shubham79 <shubhamrocks0709@gmail.com>
2023-04-09 01:31:53 +0530

    CountrySelect dropdown with site theme matching and custom option label formatting

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -5,6 +5,8 @@ import useRentModal from '@/app/hooks/useRentModal';
 import Heading from '../Heading';
 import { categories } from '../Navbar/Categories';
 import CategoryInput from '../Inputs/CategoryInput';
+import { FieldValues, useForm } from 'react-hook-form';
+import CountrySelect from '../Inputs/CountrySelect';
 
 enum STEPS {
   CATEGORY,
@@ -19,6 +21,41 @@ export default function RentModal() {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: '',
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: '',
+      price: 1,
+      title: '',
+      description: '',
+    },
+  });
+
+  const location = watch('location');
+  const category = watch('category');
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -53,8 +90,8 @@ export default function RentModal() {
           return (
             <div key={item.label} className='col-span-1'>
               <CategoryInput
-                onClick={() => {}}
-                selected={false}
+                onClick={(category) => setCustomValue('category', category)}
+                selected={category === item.label}
                 label={item.label}
                 icon={item.icon}
               />
@@ -65,15 +102,31 @@ export default function RentModal() {
     </div>
   );
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Where is your place located?'
+          subTitle='Help guests find you!'
+        />
+        <CountrySelect
+          onChange={(value) => setCustomValue('location', value)}
+          value={location}
+        />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       title='Airbnb your home!'
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      body={bodyContent}
     />
   );
 }
